8ecbcff5b3782e2d60089d0849a81cde25cb7e4b
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-01 16:32:01 +0100

    break server into pieces

--- cocktails-app/server/server.js
+++ cocktails-app/server/server.js
@@ -3,12 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000
-const app = express()
+const server = express()
 
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '../public')))
-
-app.listen(PORT, function () {
-  console.log('Listening on port', PORT)
-})
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
