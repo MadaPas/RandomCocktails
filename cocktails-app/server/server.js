b7003a562a8f7b53285c77af86a6bf48380d6e11
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-01 16:26:08 +0100

    refactor

--- cocktails-app/server/server.js
+++ cocktails-app/server/server.js
@@ -1,12 +1,13 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(bodyParser.json())
-app.use(express.static('static'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
