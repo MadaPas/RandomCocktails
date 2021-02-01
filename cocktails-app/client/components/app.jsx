cdca32540d4325d233495aaa2c8ff2a578f30d81
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-01 16:21:45 +0100

    initial setup

---
+++ cocktails-app/client/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+    <div>
+      <h1>React App!</h1>
+    </div>
+    )
+  }
+})
---
+++ cocktails-app/server/server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('static'))
+
+app.listen(PORT, function () {
+  console.log('Listening on port', PORT)
+})
