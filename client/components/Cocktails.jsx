3a5f94a1a48ea86126dd844c7233dd996cc3e4c7
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-02 13:36:27 +0100

    ignore null values

--- client/components/Cocktails.jsx
+++ client/components/Cocktails.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import request from "superagent";
@@ -6,9 +7,7 @@ const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php";
 const cocktail = "mojito";
 
 const imgStyle = {
-  width: "200px",
-  marginTop: "20px",
-  marginBottom: "20px",
+  width: "280px",
   marginRight: "15px",
 };
 class Cocktails extends React.Component {
@@ -37,7 +36,18 @@ class Cocktails extends React.Component {
           [x.strIngredient3, x.strMeasure3],
           [x.strIngredient4, x.strMeasure4],
           [x.strIngredient5, x.strMeasure5],
+          [x.strIngredient6, x.strMeasure6],
+          [x.strIngredient7, x.strMeasure7],
+          [x.strIngredient8, x.strMeasure8],
+          [x.strIngredient9, x.strMeasure9],
+          [x.strIngredient10, x.strMeasure10],
+          [x.strIngredient11, x.strMeasure11],
+          [x.strIngredient12, x.strMeasure12],
+          [x.strIngredient13, x.strMeasure13],
+          [x.strIngredient14, x.strMeasure14],
+          [x.strIngredient15, x.strMeasure15],
         ];
+
         const { strGlass } = x;
         // console.log(ingredients)
         this.setState({
@@ -60,7 +70,7 @@ class Cocktails extends React.Component {
           <img
             src={this.state.image}
             style={imgStyle}
-            alt={`image of a ${this.state.name}`}
+            alt={`image ${this.state.name}`}
           />
           <div className="container2">
             <div className="ingredients">
@@ -68,18 +78,21 @@ class Cocktails extends React.Component {
               {console.log(this.state.ingredients)}
               <ul>
                 {this.state.ingredients.map((ingredient, idx) => {
-                  return (
-                    <li key={idx}>
-                      {ingredient[1]} {ingredient[0]}
-                    </li>
-                  );
+                  if (ingredient[0] !== null && ingredient[1] !== null) {
+                    return (
+                      <li key={idx}>
+                        {ingredient[1]} {ingredient[0]}
+                      </li>
+                    );
+                  }
                 })}
               </ul>
             </div>
             <div className="instructions">
               <span id="title">How to prepare: </span>
               <div>{this.state.instructions}</div>
-            </div><br></br>
+            </div>
+            <br></br>
             <div className="glass">
               <span id="title">Glass: </span>
               <div>{this.state.glass}</div>
